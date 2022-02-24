4cf40a36c332ecd6b8831eb86e12ad37d2f888ef
razvanr91 <razvan.roman91@outlook.com>
2022-02-24 15:01:43 +0000

    Update gamesAction.js

--- src/actions/gamesAction.js
+++ src/actions/gamesAction.js
@@ -1,15 +1,19 @@
 import axios from "axios";
-import { popularGamesUrl } from "../api";
+import { newGamesUrl, popularGamesUrl, upcomingGamesUrl } from "../api";
 
 // Action Creator
 // Get data with thunk
 export const loadGames = () => async (dispatch) => {
     // Axios Fetch
     const popularGamesData = await axios.get(popularGamesUrl());
+    const newGamesData = await axios.get(newGamesUrl());
+    const upcomingGamesData = await axios.get(upcomingGamesUrl());
     dispatch({
         type: "FETCH_GAMES",
         payload: {
-            popularGames: popularGamesData.data.results
+            popularGames: popularGamesData.data.results,
+            newGames: newGamesData.data.results,
+            upcomingGames: upcomingGamesData.data.results
         }
     })
 }
