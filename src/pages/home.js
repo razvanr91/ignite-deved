6fb2875340f6bab2ad8a8fb8cbe4cf32665e6f1f
razvanr91 <razvan.roman91@outlook.com>
2022-02-25 13:54:57 +0000

    Update home.js

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,17 +1,32 @@
 import React, { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { loadGames } from "../actions/gamesAction";
+// Styling and animations
+import styled from "styled-components";
+import { motion } from "framer-motion";
+import Game from "../components/Game";
+import { useSelector } from "react-redux";
 
 const Home = () => {
     // Fetch Games
     const dispatch = useDispatch();
     useEffect(() => {
         dispatch(loadGames());
-    });
+    }, []);
 
-
+    // Get data from store
+    const {popularGames, newGames, upcomingGames} = useSelector(state => state.games);
+    console.log(popularGames)
     return (
-        <h1>Home</h1>
+        <div>
+            <h1>Home</h1>
+            <h2>Popular Games</h2>
+            {popularGames && popularGames.map(game => {
+                return (
+                    <Game />
+                )
+            })}
+        </div>
     );
 }
 
