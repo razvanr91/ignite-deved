14301456e5fb569b5177d28c6d292f9d755f68fb
razvanr91 <razvan.roman91@outlook.com>
2022-02-25 13:45:47 +0000

    Create Game.js

---
+++ src/components/Game.js
@@ -0,0 +1,15 @@
+import React from "react"
+// Styling and animations
+
+
+const Game = () => {
+    return (
+        <div>
+            <h3>Game Name</h3>
+            <p>Released date</p>
+            <p>Game Image</p>
+        </div>
+    )
+}
+
+export default Game;
